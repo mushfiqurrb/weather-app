d5b4528c0c1256d301fbb098389b42a5da67d635
Md Mushfiqur Rahman Bhuiyan <mushfiqurrb@gmail.com>
2020-11-19 15:09:37 +0600

    Api

--- seeweather/views.py
+++ seeweather/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+import json
+import requests
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=11&API_KEY=EA32BBFE-2E20-4266-8C97-961EBBCA8A0D")
+
+    try:
+        api = json.loads(api_request.content)
+
+    except Exception as e:
+        api = "ERRORRR..."
+
+
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
